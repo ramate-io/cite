286690f864c852c0aedc1a8df8791c03d3ec4068
Liam Makena Monninger <liam@ramate.io>
2025-09-03 18:19:20 +0100

    fix: cite above.

--- tests/helper-macro-git/src/lib.rs
+++ tests/helper-macro-git/src/lib.rs
@@ -1,6 +1,7 @@
 use cite::cite;
 use helper_macro_git::helper_macro_git;
 
+/// Test the git source with citation footnote
 #[helper_macro_git(doc = 1)]
 #[cite(above, reason = "Testing git source")]
 pub fn test_git_source() {
